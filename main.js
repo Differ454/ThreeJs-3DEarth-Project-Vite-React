d140501faf99f1d27d03adbe91c3be9b706c3551
DrCloudFactory454 <diru@cloudfactory.dk>
2022-08-10 23:55:15 +0200

    changes implemented

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-import { gsap } from "gsap";
+import { gsap } from 'gsap';
 //import * as THREE from "three";
 import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
-import vertexShader from "./shaders/vertex.glsl";
-import fragmentShader from "./shaders/fragment.glsl";
-import atmosphereVertexShader from "./shaders/atmosphereVertex.glsl";
-import atmosphereFragmentShader from "./shaders/atmosphereFragment.glsl";
-const texture = new THREE.TextureLoader().load("./images/globe.jpg");
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
+import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
+import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
+
 
 //console.log(vertexShader);
 
@@ -17,12 +17,12 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-//console.log(scene);
+console.log(scene);
 
 //function to render
 
 const renderer = new THREE.WebGLRenderer({
-  antialias: true, //--> to improve the image
+  antialias: true //--> to improve the image
 });
 
 renderer.setSize(innerWidth, innerHeight);
@@ -34,84 +34,85 @@ document.body.appendChild(renderer.domElement);
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
   new THREE.ShaderMaterial({
-    vertexShader,
-    fragmentShader,
-    uniforms: {
+     vertexShader,
+     fragmentShader,
+     uniforms: {
       globeTexture: {
-        value: texture
-        //value: new THREE.TextureLoader().load("globe.jpg")
-      },
-    },
+        value: new THREE.TextureLoader().load('./img/globe.jpg')
+      }
+     }
 
-    //map : new THREE.TextureLoader().load('./img/globe.jpg')
-  })
+     //map : new THREE.TextureLoader().load('./img/globe.jpg')
+ })
 );
 
-console.log(sphere);
+
+//console.log(sphere );
 // create atmosphere
 
 const atmosphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
   new THREE.ShaderMaterial({
-    vertexShader: atmosphereVertexShader,
-    fragmentShader: atmosphereFragmentShader,
-    blending: THREE.AdditiveBlending,
-    side: THREE.BackSide,
-  })
+     vertexShader : atmosphereVertexShader,
+     fragmentShader : atmosphereFragmentShader,
+     blending: THREE.AdditiveBlending,
+     side: THREE.BackSide
+ })
 );
-
-atmosphere.scale.set(1.1, 1.1, 1.1);
+ 
+atmosphere.scale.set(1.1, 1.1, 1.1)
 scene.add(atmosphere);
 
-const group = new THREE.Group();
-group.add(sphere);
-scene.add(group);
+const group = new THREE.Group()
+group.add(sphere)
+scene.add(group)
 //console.log(atmosphere );
 
-const starGeometry = new THREE.BufferGeometry();
-const starMaterial = new THREE.PointsMaterial({ color: 0xffffff });
+const starGeometry = new THREE.BufferGeometry()
+const starMaterial = new THREE.PointsMaterial({color: 0xffffff})
 
-const starVertices = [];
+const starVertices = []
 for (let i = 0; i < 10000; i++) {
-  const x = (Math.random() - 0.5) * 2000;
-  const y = (Math.random() - 0.5) * 2000;
-  const z = -Math.random() * 2000;
-  starVertices.push(x, y, z);
+  const x =  (Math.random() - 0.5) * 2000
+  const y =  (Math.random() - 0.5) * 2000
+  const z = -Math.random() * 2000
+  starVertices.push(x, y, z)
 }
-//console.log(starVertices);
-starGeometry.setAttribute(
-  "position",
-  new THREE.Float32BufferAttribute(starVertices, 3)
-);
+console.log(starVertices);
+starGeometry.setAttribute('position',
+new THREE.Float32BufferAttribute(starVertices, 3))
 
-const stars = new THREE.Points(starGeometry, starMaterial);
-//console.log(stars)
-scene.add(stars);
+const stars = new THREE.Points(starGeometry, starMaterial)
+console.log(stars)
+scene.add(stars)
 
 camera.position.z = 15;
 
 const mouse = {
   x: undefined,
-  y: undefined,
-};
+  y: undefined
+
+}
 
 //animate function
 const animate = () => {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  sphere.rotation.y += 0.003;
-  gsap.to(group.rotation, {
-    x: -mouse.y * 0.5,
-    y: mouse.x * 1.4,
-    duration: 2,
-  });
+  sphere.rotation.y += 0.003
+    gsap.to(group.rotation, {
+      x: -mouse.y * 0.5,
+      y: mouse.x * 1.4,
+      duration: 2  
+    })
 };
 
 animate();
 
 // function to move the mouse
 
-addEventListener("mousemove", () => {
+addEventListener('mousemove', () => {
   mouse.x = (event.clientX / innerWidth) * 2 - 1;
   mouse.y = -(event.clientY / innerHeight) * 2 + 1;
-});
+  
+})
+
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite'
 import vitePluginString from "vite-plugin-string";
 
-export default defineConfig({
+export default {
   plugins: [vitePluginString.default()],
-});
+};
